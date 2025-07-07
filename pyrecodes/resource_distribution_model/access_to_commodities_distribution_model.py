f2bdaf1563e49d1053e35dda99218b381362e035
Nikola Blagojević <blagojevic@ibk.baug.ethz.ch>
2025-07-07 13:33:46 -0700

    updated access_to_commodities_distribution_model to be more efficient and rely on the transfer service distribution model instead of simulating traffic flow by itself

--- pyrecodes/resource_distribution_model/access_to_commodities_distribution_model.py
+++ pyrecodes/resource_distribution_model/access_to_commodities_distribution_model.py
@@ -1,33 +1,44 @@
 from pyrecodes.resource_distribution_model.residual_demand_traffic_distribution_model import ResidualDemandTrafficDistributionModel
+from pyrecodes.resource_distribution_model.abstract_resource_distribution_model import AbstractResourceDistributionModel
 from pyrecodes.component.component import Component
 from pyrecodes.component.r2d_component import R2DBuildingWithBusiness
 
-class AccessToCommoditiesDistributionModel(ResidualDemandTrafficDistributionModel):
+class AccessToCommoditiesDistributionModel(AbstractResourceDistributionModel):
     """
     AccessToCommoditiesDistributionModel checks whether a business has access to comodities provided by suppliers.
     Each business has a list of local suppliers and can also have suppliers outside the local area.
-    The model builds on the ResidualDemandTrafficDistributionModel, but it is not a traffic model.
+    The model is connected to the ResidualDemandTrafficDistributionModel.
     It uses the traffic flow simulator to check whether the business has access to suppliers.
     """
+   
+    def set_transfer_service_distribution_model(self, transfer_service_distribution_model: ResidualDemandTrafficDistributionModel) -> None:
+        self.transfer_service_distribution_model = transfer_service_distribution_model
+        self.check_supplier_trips_in_od_matrix()
 
-    def __init__(self, resource_name: str, resource_parameters: dict, components: list[Component]):
-        super().__init__(resource_name, resource_parameters, components)
-        
-    def distribute(self, time_step: int) -> None:
-        super().distribute(time_step)
-        self.update_access_to_suppliers()
-
-    def update_access_to_suppliers(self) -> None:        
+    def check_supplier_trips_in_od_matrix(self) -> None:
         for component in self.components:
-            # TODO: @Tinger please check this condition
             if isinstance(component, R2DBuildingWithBusiness):
-                component.update_access_of_businesses_to_suppliers(self.building_to_traffic_node_dict, self.travel_times[-1], self.travel_time_change)  
-                # TODO: @Tinger please add this method to the R2DBuildingWithBusiness class
-                # check out the update_buildings_traffic_situation method in the ResidualDemandTrafficDistributionModel
-                # once you have the travel time change factor for traveling from the business to all the supplier
-                # see if any one of those is below the cutoff threshold and that will give you the access to suppliers
-                # we might need to modify the od matrix to include the suppliers as well
-                # component.update_access_of_businesses_to_suppliers(self.travel_times[-1])
+                for business in component.businesses:
+                    business.check_supplier_trips_in_od_matrix(self.transfer_service_distribution_model)
 
-    
+    def distribute(self, time_step: int) -> None:
+        self.update_access_to_suppliers(time_step,
+                                        self.transfer_service_distribution_model)      
+
+    def update_access_to_suppliers(self, time_step, transfer_service_distribution_model) -> None:
+        for component in self.components:
+            if isinstance(component, R2DBuildingWithBusiness):
+                component.update_access_of_businesses_to_suppliers(time_step, transfer_service_distribution_model)
+
+    def get_total_supply(self, scope: str) -> float:
+        # TODO: implement this
+        return 0
+
+    def get_total_demand(self, scope: str) -> float:
+        # TODO: implement this
+        return 0
+
+    def get_total_consumption(self, scope: str) -> float:
+        # TODO: implement this
+        return 0
       
