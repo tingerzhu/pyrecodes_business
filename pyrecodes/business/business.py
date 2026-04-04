6a8559cac75f0b3fdb0c8771d8bf985f9ba5c517
Nikola Blagojević <blagojevic@ibk.baug.ethz.ch>
2026-04-03 17:19:05 -0700

    is_building_accessible moved out of the Business class into transfer service distribution classes. Multiple transfer service models are now available to each business. Use IslandConnectivity model when checking access to buildings on mainland and residual_demand for checking the connectivity between buildings on the island.

--- pyrecodes/business/business.py
+++ pyrecodes/business/business.py
@@ -1,10 +1,16 @@
 from pyrecodes.component.component import Component
 from pyrecodes.component.r2d_component import R2DBuilding
-import pandas as pd
 from pyrecodes.resource_distribution_model.residual_demand_traffic_distribution_model import ResidualDemandTrafficDistributionModel
 
-TRAVEL_TIME_CUTOFF = 14400  # four hours in seconds, used to determine if a business has access to suppliers
-TRAVEL_TIME_CHANGE_CUTOFF = 4  # if the difference between pre and post disaster travel times is greater than this, the business has no access to suppliers
+
+def is_building_accessible(transfer_service_models: list, time_step: int,
+                           origin_building_id: str, destination_building_id: str) -> bool:
+    for model in transfer_service_models:
+        result = model.is_building_accessible(time_step, origin_building_id, destination_building_id)
+        if result is not None:
+            return result
+    return False
+
 
 class Business():
 
@@ -40,46 +46,27 @@ class Business():
                 if component.aim_id in self.parameters['EmployeeLocations']:
                     self.employee_homes.append(component)
 
-    def check_employees(self, time_step: int, traffic_flow_model: ResidualDemandTrafficDistributionModel) -> None:
+    def check_employees(self, time_step: int, transfer_service_models: list) -> None:
         employee_available = 0
         for employee_home in self.employee_homes:
-            if employee_home.functionality_level == 1.0 and self.is_building_accessible(time_step, traffic_flow_model, employee_home.aim_id, self.home_component.aim_id):
+            if employee_home.functionality_level == 1.0 and is_building_accessible(transfer_service_models, time_step, employee_home.aim_id, self.home_component.aim_id):
                 employee_available += 1
 
         self.employees_available[time_step] = employee_available / self.parameters['NumEmployees']
         self.update_current_business_functionality(time_step, self.employees_available[time_step], 'Labor')
 
-    def is_building_accessible(self, time_step: int, transfer_service_distribution_model: ResidualDemandTrafficDistributionModel,
-                               origin_building_id: str, destination_building_id: str) -> bool:
-        origin_node = transfer_service_distribution_model.building_to_traffic_node_dict.get(origin_building_id, None)
-        destination_node = transfer_service_distribution_model.building_to_traffic_node_dict.get(destination_building_id, None)
-        if origin_node is None or destination_node is None:
-            return False
-        isolated_nodes = transfer_service_distribution_model.od_trip_checker.isolated_nodes
-        if origin_node in isolated_nodes or destination_node in isolated_nodes:
-            return False
-        travel_times, trip_index, change_index = self.get_latest_travel_times(transfer_service_distribution_model, time_step)
-        return self.check_accessibility(origin_node, destination_node, travel_times, trip_index, change_index)
-
-    def get_latest_travel_times(self, transfer_service_distribution_model: ResidualDemandTrafficDistributionModel, time_step: int):
-        last_distribution_time_step = transfer_service_distribution_model.find_nearest_distribution_time_step(time_step)
-        travel_times = transfer_service_distribution_model.travel_times[last_distribution_time_step]
-        trip_index = transfer_service_distribution_model.trip_index
-        change_index = transfer_service_distribution_model.travel_time_change_index[last_distribution_time_step]
-        return travel_times, trip_index, change_index
-        
     def get_employee_demand(self) -> float:
         return self.parameters['NumEmployees'] if self.business_functionality_level > 0 else 0
-        
+
     def get_employee_supply(self) -> float:
         if not self.employees_available:
             return 0
         last_time_step = max(self.employees_available)
         return int(self.employees_available[last_time_step] * self.parameters['NumEmployees'])
-    
+
     def get_employee_consumption(self) -> float:
         return min(self.get_employee_demand(), self.get_employee_supply())
-    
+
     def update(self, time_step:int) -> None:
         """
         Update the business.
@@ -87,7 +74,7 @@ class Business():
         self.business_functionality_level = 1.0
         self.reason_for_drop[time_step] = []
         self.update_current_business_functionality(time_step, self.home_component.functionality_level, 'Home Component Functionality')
-        
+
     def update_functionality_based_on_unmet_demand(self, time_step, percent_of_met_demand: float) -> None:
         """
         Update the functionality of the business based on the unmet demand.
@@ -103,10 +90,10 @@ class Business():
         self.revenue[time_step] = min(current, self.revenue[time_step]) if time_step in self.revenue else current
 
     def update_customer_base(self, time_step: int, customer_base_population_ratios: dict,
-                             transfer_service_distribution_model=None) -> None:
+                             transfer_service_models: list = None) -> None:
         total_customer_base_ratio = 0
         outside_island_accessible = self.check_outside_island_customer_accessibility(
-            time_step, transfer_service_distribution_model)
+            time_step, transfer_service_models)
         for block in self.parameters['VisitorHomeCBGs'].keys():
             if block == 'Others':
                 if outside_island_accessible:
@@ -118,18 +105,14 @@ class Business():
         self.update_current_business_functionality(time_step, total_customer_base_ratio, 'Customer Base')
 
     def check_outside_island_customer_accessibility(self, time_step: int,
-                                                    transfer_service_distribution_model=None) -> bool:
+                                                    transfer_service_models: list = None) -> bool:
         outside_island_buildings = self.parameters.get('OutsideIslandCustomerBuildings', [])
-        if not outside_island_buildings or transfer_service_distribution_model is None:
+        if not outside_island_buildings or not transfer_service_models:
             return True
         for building_id in outside_island_buildings:
-            if self.is_building_accessible(time_step, transfer_service_distribution_model, self.home_component.aim_id, building_id):
+            if is_building_accessible(transfer_service_models, time_step, self.home_component.aim_id, building_id):
                 return True
         return False
-
-    def check_customer_base_outside_island_trips_in_od_matrix(self, transfer_service_distribution_model: ResidualDemandTrafficDistributionModel) -> None:
-        outside_island_buildings = self.parameters.get('OutsideIslandCustomerBuildings', [])
-        self.check_trips_in_od_matrix(transfer_service_distribution_model, outside_island_buildings)
 
     def update_current_business_functionality(self, time_step: int, updated_level: float, reason_for_drop: str) -> None:
         if self.business_functionality_level > updated_level:
@@ -146,7 +129,7 @@ class Business():
             if component_closest_node is None:
                 print(f"Component {component_id} not found in building_to_traffic_node_dict")
             else:
-                if not (transfer_service_distribution_model.od_trip_checker.check_trip_in_od_matrix(component_closest_node, business_closest_node) or 
+                if not (transfer_service_distribution_model.od_trip_checker.check_trip_in_od_matrix(component_closest_node, business_closest_node) or
                         transfer_service_distribution_model.od_trip_checker.check_trip_in_od_matrix(business_closest_node, component_closest_node)):
                     transfer_service_distribution_model.od_trip_checker.add_to_od_matrix(component_closest_node, business_closest_node)
 
@@ -156,23 +139,10 @@ class Business():
     def check_employee_trips_in_od_matrix(self, transfer_service_distribution_model: ResidualDemandTrafficDistributionModel) -> None:
         self.check_trips_in_od_matrix(transfer_service_distribution_model, self.parameters['EmployeeLocations'])
 
-    def update_access_to_suppliers(self, time_step, transfer_service_distribution_model: ResidualDemandTrafficDistributionModel) -> None:
+    def update_access_to_suppliers(self, time_step, transfer_service_models: list) -> None:
         any_accessible = any(
-            self.is_building_accessible(time_step, transfer_service_distribution_model, self.home_component.aim_id, supplier)
+            is_building_accessible(transfer_service_models, time_step, self.home_component.aim_id, supplier)
             for supplier in self.parameters['NearestRetailLocations']
         )
         if not any_accessible:
             self.update_current_business_functionality(time_step, 0, 'LocalSuppliers')
-
-    def check_accessibility(self, origin_node, destination_node, travel_times: pd.DataFrame, trip_index: dict, change_index: dict) -> bool:
-        if origin_node == destination_node:
-            return True
-        row = trip_index.get((origin_node, destination_node))
-        if row is None:
-            row = trip_index.get((destination_node, origin_node))
-        if row is None:
-            return False
-        record = travel_times.iloc[row]
-        travel_time = record['travel_time_used']
-        travel_time_change_factor = change_index.get(record['agent_id'], float('inf'))
-        return travel_time_change_factor <= TRAVEL_TIME_CHANGE_CUTOFF and travel_time <= TRAVEL_TIME_CUTOFF
