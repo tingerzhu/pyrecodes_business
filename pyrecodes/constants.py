5f23c594ca089e752a374f6bddafd3b10394a6b9
Nikola Blagojević <blagojevic@ibk.baug.ethz.ch>
2026-06-14 15:38:28 -0700

    move time steps in a year definition to constants - impportant for whether we use week or day as time step to deaggregate revenue

--- pyrecodes/constants.py
+++ pyrecodes/constants.py
@@ -1,7 +1,14 @@
 INCH_TO_MILE = 63360
 FEET_TO_MILE = 5280
 METER_TO_MILE = 1609.34
-SECONDS_IN_TIME_STEP = 3600 
+SECONDS_IN_TIME_STEP = 3600
+
+# --- Business revenue time resolution ----------------------------------------------------
+# A business' SalesVolume is an annual figure. The revenue earned per simulation time step is
+# SalesVolume / TIME_STEPS_IN_A_YEAR. Single knob for the revenue time resolution:
+# 52 -> weekly time steps; 365 -> daily time steps.
+TIME_STEPS_IN_A_YEAR = 52
+
 GANTT_BAR_DISTANCE = 10
 GANTT_BAR_WIDTH = 5
 LOR_ALPHA = 0.2
